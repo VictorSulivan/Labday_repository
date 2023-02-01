408852ca74b4054ec57ff368f03456e856ea32e3
nom1 <nom1@gmail.com>
2023-02-01 10:21:08 +0100

    mise a jour zinebe

--- src/class/UserManager.php
+++ src/class/UserManager.php
@@ -8,10 +8,13 @@ class UserManager {
 	}
 
 	public function insert(User $user) {
-		$stmh = $this->db->prepare('INSERT INTO users(fullname, email, password, role, last_ip) VALUES(?, ?, ?, ?, ?)');
+
+
+		$stmh = $this->db->prepare('INSERT INTO user(nom, prenom, deuxieme_prenom, role, adresse_domicile, date_de_naissance, telephone, status_marital, status_vital,	numero_secu_social, email, password) VALUES(?, ?, ?, role, ?, ?Created_AT, ?, ?, ?,	?, ?, ?)');
 		$stmh->execute([
-			$user->fullname, $user->email, $user->password, $user->role, $user->last_ip
+			$user->nom, $user->prenom, $user->deuxieme_prenom, $user->role, $user->role, $user->adresse_domicile, $user->date_de_naissance, $user->telephone, $user->status_vital, $user->numero_secu_social, $user->email,  $user->password
 		]);
+		
 		return $this->db->lastInsertId();
 	}
 
--- src/templates/template.php
+++ src/templates/template.php
@@ -9,13 +9,13 @@
 	<link rel="stylesheet" href="assets/CSS/header.css">
   </head>
   <body>
-    
-	<div>
-		<?php require_once __DIR__ . '/partials/menu.php'; ?>
-	</div>  
-	
+  	<div class="divheader">
+		<?php require_once __DIR__ . '/partials/header.php'; ?>
+	</div>   
 	<?= $page_content ;?>
 	<?= $page_scripts ;?>
+	
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-	</body>
+	<?php require_once __DIR__ . '/partials/footer.php'; ?>
+</body>
 </html>
