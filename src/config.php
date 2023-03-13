e29412860854f453e24d5900714c45f5cc3a6ec9
Soullessoup <116712568+Soullessoup@users.noreply.github.com>
2023-03-13 14:32:26 +0100

    zineb

---
+++ src/config.php
@@ -0,0 +1,29 @@
+<?php
+
+$config = [];
+$config['db'] = [
+    'host' => 'localhost',
+    'name' => 'projet_labday',
+    'user' => 'root',
+    'pass' => 'root',
+    'port' => 3306
+
+];
+
+$config['roles'] = [
+    0 => "Utilisateur Banni",
+    1 => "Utilisateur Non-Vérifié",
+    10 => "Utilisateur Vérifié",
+    20 => "Soins",
+    30 => "Etudes",
+    40 => "judicaire",
+    200 => "Administrations",
+    1000 => "Admin"
+
+];
+
+$config['status'] = [
+    0 => "Opération refusée",
+    50 => "Opération en cours d'analyse",
+    100 => "Opération effectuée"
+];
