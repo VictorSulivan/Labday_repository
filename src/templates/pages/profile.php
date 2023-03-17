32a2f50751f04568c1df9cfb1f3560c28f91a36b
Soullessoup <116712568+Soullessoup@users.noreply.github.com>
2023-03-17 10:31:09 +0100

    css profil

--- src/templates/pages/profile.php
+++ src/templates/pages/profile.php
@@ -4,37 +4,50 @@ require_once __DIR__ . '/../../init.php';
 
 $page_title = "profil user!";
 
+
+$head_metas = "<link rel=stylesheet href=assets/CSS/profil.css>";
+
+
 ob_start();
 
 ?>
 
-<h1>Page d'accueil</h1>
 
 <div>
-    <div>
-        <div>
-            <h3>
-                Information de profil
-            </h3>
-        </div>
-        <div>
-            <!--phto du profil
-                <img src="" alt="">-->
-                <p>photo</p>
+    <h1>Page de profil (photo de profil)</h1>
+
+
+    <div class="Info_profil">
+        <div class="content">
+            <div>
+                <h3>
+                    Information de profil
+                </h3>
+            </div>
+            <div> 
+                <!--phto du profil
+                    <img src="" alt="">-->
+                    <p>ph</p>
+            </div>
         </div>
     </div>
-    <div>
-        <div>
-            <h4>
-                Informations principales
-            </h4>
-            <p>info secondaire</p>
+
+    <div class="second">
+        <div class="div">
+            <div class="content">
+                <h4>
+                    Informations principales
+                </h4>
+                <p>info secondairegfhvjbknbhgvycftdxrctvyubino,nubyuvtcrxewz</p>
+            </div>
         </div>
-        <div>
-            <h4>
-                Informations secondaires
-            </h4>
-            <p>info secondaire</p>
+        <div class="div">
+            <div class="content">
+                <h4>
+                    Informations secondaires
+                </h4>
+                <p>info secondaire</p>
+            </div>
         </div>
     </div>
 </div>
